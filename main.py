96ac1b73aa9ee5d4acf46786425ed2ee82d35922
neerajaryaai <neeraj.singh@arya.ai>
2024-10-15 00:46:46 +0530

    play with build celebrity search application using stremlit

---
+++ main.py
@@ -0,0 +1,19 @@
+## Integrate our code OpenAI API
+import os
+from constants import openai_key
+from langchain_community.llms import OpenAI
+
+import streamlit as st
+
+os.environ["OPENAI_API_KEY"] = openai_key
+
+# streamlit framework
+st.title('Langchain Demo with OpenAI API')
+input_text = st.text_input("Search the topic you want")
+
+## OPENAI LLMs 
+llm = OpenAI(temperature=0.8)
+
+
+if input_text:
+    st.write(llm(input_text))
